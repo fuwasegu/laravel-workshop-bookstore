8dc5116bc717219d3080f41edfe703f1790d3f2e
HirosuguTakeshita <h_takeshita@yumemi.co.jp>
2025-03-27 10:05:44 +0900

    class への切り出し

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Book\CreateControllerA;
 use App\Http\Controllers\Book\CreateControllerB;
 use App\Http\Controllers\Book\CreateControllerC;
 use App\Http\Controllers\Book\CreateControllerD;
+use App\Http\Controllers\Book\CreateControllerE;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\BookStockController;
 
@@ -26,3 +27,4 @@ Route::post('/books/create-a', CreateControllerA::class);
 Route::post('/books/create-b', CreateControllerB::class);
 Route::post('/books/create-c', CreateControllerC::class);
 Route::post('/books/create-d', CreateControllerD::class);
+Route::post('/books/create-e', CreateControllerE::class);
